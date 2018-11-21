18516b74f46ad18f5a4ea4ab83862d072c108673
lucas-lucena <lucas.lucena@ccc.ufcg.edu.br>
2018-11-21 12:01:45 -0300

    Iniciando os testes de Controller

---
+++ projectWorkspace/eDoe/tests/ControllerTest.java
@@ -0,0 +1,64 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import eDoe.Controller;
+
+class ControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testAdicionaDoador() {
+		
+	}
+	
+	@Test
+	void testPesquisaUsuarioPorId() {
+		
+	}
+	
+	@Test
+	void testPesquisaUsuarioPorNome() {
+		
+	}
+	
+	@Test
+	void testAtualizaUsuario() {
+		
+	}
+	
+	@Test
+	void testRemoveUsuario() {
+		
+	}
+	
+	@Test
+	void testAdicionaDescritor() {
+		
+	}
+	
+	@Test
+	void testAdicionaItemParaDoacao() {
+		
+	}
+	
+	@Test
+	void testExibeItem() {
+		
+	}
+	
+	@Test
+	void testAtualizaItemParaDoacao() {
+		
+	}
+	
+	@Test
+	void testRemoveItemParaDoacao() {
+		
+	}
+
+}
